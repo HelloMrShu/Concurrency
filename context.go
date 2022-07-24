c308b267f12380a5f2bb2ef04f5603c0a20b144a
tinker <gaoqi198@163.com>
2022-07-24 20:27:43 +0800

    feat: waitgroup modify

---
+++ context.go
@@ -0,0 +1,17 @@
+package main
+
+type Job struct {
+
+}
+
+type Worker struct {
+
+}
+
+type Workspace struct {
+
+}
+
+func main() {
+
+}
--- waitgroup.go
+++ waitgroup.go
@@ -2,27 +2,36 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"sync"
 	"time"
 )
 
+//waitGroup 适用于一组goroutine协同完成一个任务，当全部的goroutine都完成，任务才完成
+
+/*
+ * 例如一个服务需要调用底层多个服务的数据，那么起多个goroutine分别来调用每个服务来获取数据
+ * 当全部goroutine都完成时，上层服务再统一处理各个服务的数据
+*/
+var wg sync.WaitGroup
+
+func rpc1() {
+	fmt.Println("rpc 1")
+	wg.Done()
+}
+
+func rpc2() {
+	fmt.Println("rpc 2")
+	wg.Done()
+}
+
 func main() {
-	url := "https://www.sohu.com"
-	wg := &sync.WaitGroup{}
-	for i:= 0; i < 10; i++ {
-		wg.Add(1)
-		go func(i int, url string) {
-			defer wg.Done()
-			fmt.Println("goroutine: ", i)
-			client := http.Client{Timeout: 20 * time.Second}
-			resp, err := client.Get(url)
-			if err != nil {
-				fmt.Println("request error", err, resp)
-			}
-		}(i, url)
-	}
-	time.Sleep(1 * time.Second)
+	wg.Add(2)
+
+	go rpc1()
+	go rpc2()
+
 	wg.Wait()
+
+	time.Sleep(1 * time.Second)
 	fmt.Println("task is done")
 }
